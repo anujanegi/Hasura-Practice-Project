0cb3d59252b52db31d754226c8ce47cf67e8e83e
anujanegi <anujanegi1@gmail.com>
2017-12-31 18:52:17 +0530

    Login updated

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -14,4 +14,26 @@ def login():
 
 @app.route("/signup", methods=['POST'])
 def signup():
-    return "done"
+    # This is the url to which the query is made
+    url = "https://auth.analogically97.hasura-app.io/v1/signup"
+
+    # This is the json payload for the query
+    requestPayload = {
+        "provider": "username",
+        "data": {
+            "username": "johnsmith",
+            "password": "js@hasura"
+        }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+    # resp.content contains the json response.
+    print resp.content
+    return resp.content
