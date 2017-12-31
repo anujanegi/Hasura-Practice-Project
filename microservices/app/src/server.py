f900291e8c21459e163e69d12ee3a72487121c53
anujanegi <anujanegi1@gmail.com>
2017-12-31 17:43:11 +0530

    Login api test added

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -12,6 +12,23 @@ def home():
 def login():
     return render_template('login.html')
 
+@views.route('/signup', methods=['POST'])
+def signup():
+    #API URL
+    url = request.url_root + url_for('api.login')[1:]
+    try:
+        data = json.loads(json.dumps(request.form))
+        r = requests.post(url, json = data)
+        res = r.json()
+        if res["code"] == 200:
+            #Set session for this blueprint
+            session["user-token"] = res["args"]
+            return redirect('/')
+        else:
+            return redirect('/')
+    except Exception as e:
+        return str(e)
+
 # Uncomment to add a new URL at /new
 
 # @app.route("/json")
