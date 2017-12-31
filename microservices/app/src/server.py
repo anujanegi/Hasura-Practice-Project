0f365b1563ab7f13ecd8947e2f191def2b30c6e8
anujanegi <anujanegi1@gmail.com>
2017-12-31 18:37:26 +0530

    Login api fixed

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,8 +1,6 @@
 from src import app
-from flask import render_template
-import requests
-import json
-# from flask import jsonify
+from flask import render_template, json, requests
+from flask import jsonify
 
 
 @app.route("/")
@@ -13,30 +11,7 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('/signup', methods=['POST'])
+@app.route("/signup", methods=['POST'])
 def signup():
-    url = "https://auth.analogically97.hasura-app.io/v1/signup"
 
-    username = request.form['username']
-    password = request.form['pswd']
-
-    # This is the json payload for the query
-    requestPayload = {
-        "provider": "username",
-        "data": {
-            "username": "anuja",
-            "password": "anuja123"
-        }
-    }
-
-    # Setting headers
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    # Make the query and store response in resp
-    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
-
-    # resp.content contains the json response.
-    print resp.content
     return "done"
