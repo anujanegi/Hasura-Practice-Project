13a5faf4c61e593a4baaa7c6b15dac2b8bb15e6e
anujanegi <anujanegi1@gmail.com>
2017-12-31 18:45:31 +0530

    Login updated

--- microservices/app/src/static/js/login.js
+++ microservices/app/src/static/js/login.js
@@ -3,10 +3,10 @@ window.onload = function () {
   sign_up = document.getElementById('signup');
   form = document.getElementById('myForm');
   sign_in.onclick = function () {
-    form.action = "{{ url_for('signup') }}"
+    form.action = "signin"
   }
 
   sign_up.onclick = function () {
-   form.action = "{{ url_for('signup') }}"
+   form.action = "/signup
  }
 }
