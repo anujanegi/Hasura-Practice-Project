9461980096a1666fdb4f20937715d584016a1987
anujanegi <anujanegi1@gmail.com>
2018-01-01 20:54:09 +0530

    Testing form action

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,5 +1,5 @@
 from src import app
-from flask import render_template, request
+from flask import render_template, request, json
 import requests
 import json
 
@@ -14,4 +14,28 @@ def login():
 
 @app.route("/signup", methods=['POST'])
 def signup():
-    return (request.form['username'])
+    username = request.form['username']
+    pswd = request.form['pswd']
+    # This is the url to which the query is made
+    url = "https://auth.analogically97.hasura-app.io/v1/signup"
+
+    # This is the json payload for the query
+    requestPayload = {
+        "provider": "username",
+        "data": {
+            "username": "johnsmith",
+            "password": "js@hasura"
+        }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+    # resp.content contains the json response.
+    print resp.content
+    return resp.content
