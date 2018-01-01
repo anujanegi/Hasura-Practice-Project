d1e4bd3e5c27a8c3d34ff2cb7b2a1482d8456803
anujanegi <anujanegi1@gmail.com>
2018-01-01 22:16:48 +0530

    Sign in added

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -16,6 +16,7 @@ def login():
 def signup():
     username = request.form['username']
     pswd = request.form['pswd']
+
     # This is the url to which the query is made
     url = "https://auth.analogically97.hasura-app.io/v1/signup"
 
@@ -25,6 +26,37 @@ def signup():
         "data": {
             "username": username,
             "password": pswd
+        }
+    }
+
+    # Setting headers
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Make the query and store response in resp
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers)
+
+    # resp.content contains the json response.
+    print(resp.content)
+    return (resp.content)
+
+@app.route("/signin", methods=['POST'])
+def signin():
+    username = request.form['username']
+    pswd = request.form['pswd']
+
+    # This is the url to which the query is made
+    url = "https://auth.analogically97.hasura-app.io/v1/signup"
+    # This is the url to which the query is made
+    url = "https://auth.analogically97.hasura-app.io/v1/login"
+
+    # This is the json payload for the query
+    requestPayload = {
+        "provider": "username",
+        "data": {
+            "username": "nsmith",
+            "password": "js@hasura"
         }
     }
 
